3ac2c8fdfdb533d74de01988ff941940d4bc0c61
Yamkela Jack <yamkelajack06@gmail.com>
2025-09-20 21:11:11 +0200

    Little update

--- src/pages/Main/HomePage.jsx
+++ src/pages/Main/HomePage.jsx
@@ -1,6 +1,5 @@
 import Header from "../../components/header";
 import Timetable from "../../components/Timetable";
-import style from "../../styles/homepage.module.css";
 
 const HomePage = () => {
   return (
