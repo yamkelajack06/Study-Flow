78963891e457aba06281f0ece98da8bf3007840f
Yamkela Jack <yamkelajack06@gmail.com>
2026-02-17 18:37:53 +0200

    Finalized firebase auth and storage

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,16 +1,16 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { initializeUI } from '@firebase-oss/ui-core';
+import { initializeUI } from "@firebase-oss/ui-core";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGE_SENDER_ID, 
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID 
+  apiKey: import.meta.env.VITE_FIRE_BASE_AUTH_API_KEY,
+  authDomain: import.meta.env.VITE_FIRE_BASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIRE_BASE_AUTH_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIRE_BASE_AUTH_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIRE_BASE_AUTH_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIRE_BASE_AUTH_APP_ID,
+  measurementId: import.meta.env.VITE_FIRE_BASE_AUTH_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
